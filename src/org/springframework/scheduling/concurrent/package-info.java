21e8a042732fa1a0aa8f49916d72f8b8ef165c67
Edward Lee <bert825_work@163.com>
2014-09-20 00:30:21 +0800

    [译] org.springframework.scheduling.concurrent包描述
    提供支持JDK
    1.5+并发包(java.util.concurrent)中的<b>执行器机制</b>的调度工具类，允许在Spring上下文中设置一个线程池执行器(ThreadPoolExecutor或ScheduledThreadPoolExecutor)作为一个bean。

--- src/org/springframework/scheduling/concurrent/package-info.java
+++ src/org/springframework/scheduling/concurrent/package-info.java
@@ -1,6 +1,9 @@
 /**
+ * 提供支持JDK 1.5+并发包(java.util.concurrent)中的<b>执行器机制</b>的调度工具类，
+ * 允许在Spring上下文中设置一个线程池执行器(ThreadPoolExecutor或ScheduledThreadPoolExecutor)
+ * 作为一个bean。
  *
- * Scheduling convenience classes for the JDK 1.5+ Executor mechanism
+ * <p>Scheduling convenience classes for the JDK 1.5+ Executor mechanism
  * in the {@code java.util.concurrent} package, allowing to set up a
  * ThreadPoolExecutor or ScheduledThreadPoolExecutor as a bean in a Spring
  * context. Provides support for the native {@code java.util.concurrent}
