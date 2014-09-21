37dd9f9d6846c886beb96f208cae5dccb8aa4616
Edward Lee <bert825_work@163.com>
2014-09-21 14:59:06 +0800

    [译] 调度包——Spring调度支持的通用条款，独立于任何特定的调度系统
    SchedulingAwareRunnable：Runnable接口的扩展，增加长时间运行操作的特殊回调。
    SchedulingTaskExecutor：任务执行器的扩展，暴露与相关的潜在任务提交者的调度特性。
    TaskScheduler：任务调度器接口，抽象基于不同触发器的可运行任务的调度。
    Trigger：触发器对象的公共接口，确定相关任务的下一次执行时间。
    TriggerContext：封装一个给定任务的最后执行时间和最后完成时间的上下文对象。

--- src/main/java/org/springframework/scheduling/package-info.java
+++ src/main/java/org/springframework/scheduling/package-info.java
@@ -1,7 +1,16 @@
 
 /**
+ * Spring调度支持的通用条款，独立于任何特定的调度系统。
+ * 
+ * <ul>
+ * <li>SchedulingAwareRunnable：Runnable接口的扩展，增加长时间运行操作的特殊回调。
+ * <li>SchedulingTaskExecutor：任务执行器的扩展，暴露与相关的潜在任务提交者的调度特性。
+ * <li>TaskScheduler：任务调度器接口，抽象基于不同触发器的可运行任务的调度。
+ * <li>Trigger：触发器对象的公共接口，确定相关任务的下一次执行时间。
+ * <li>TriggerContext：封装一个给定任务的最后执行时间和最后完成时间的上下文对象。
+ * </ul>
  *
- * General exceptions for Spring's scheduling support,
+ * <p>General exceptions for Spring's scheduling support,
  * independent of any specific scheduling system.
  *
  */
