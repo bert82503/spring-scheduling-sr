b2d25ace96bcdbbb94cb3bfd444315879912f7b1
Edward Lee <bert825_work@163.com>
2014-09-23 22:44:17 +0800

    [译] org.springframework.util包——各种实用工具类，如字符串操作工具、一个分页对象的状态持有者

---
+++ src/main/java/org/springframework/util/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * 各种实用工具类，如字符串操作工具、一个分页对象的状态持有者。
+ *
+ * <p>Miscellaneous utility classes, such as String manipulation utilities,
+ * a Log4J configurer, and a state holder for paged lists of objects.
+ *
+ */
+package org.springframework.util;
+
