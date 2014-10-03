df5745c82750496221dcea8ca5320c0d939683ec
Edward Lee <bert825_work@163.com>
2014-10-03 16:07:59 +0800

    [译] org.springframework.core.task.support包——Spring的任务执行器(TaskExecutor)抽象支持类集
    包括一个标准的执行器服务(ExecutorService)接口的适配器。
    
    TaskExecutorAdapter：任务执行器(TaskExecutor)接口的适配器
    ExecutorServiceAdapter：标准的执行器服务(ExecutorService)接口的适配器
    ConcurrentExecutorAdapter：并发执行器的适配器

---
+++ src/main/java/org/springframework/core/task/support/package-info.java
@@ -0,0 +1,11 @@
+
+/**
+ * Spring的任务执行器(TaskExecutor)抽象支持类集，
+ * 包括一个标准的执行器服务(ExecutorService)接口的适配器。
+ *
+ * <p>Support classes for Spring's TaskExecutor abstraction.
+ * Includes an adapter for the standard ExecutorService interface.
+ *
+ */
+package org.springframework.core.task.support;
+
