59adf3ea75be894215e5a040aa4766223496ae21
Edward Lee <bert825_work@163.com>
2014-10-05 16:47:50 +0800

    ForkJoinPoolFactoryBean——一个构建并暴露一个预先配置的"Fork-Join"任务池(ForkJoinPool)的Spring工厂Bean(FactoryBean)
    继承自FactoryBean<ForkJoinPool>，包装ForkJoinPool所有属性及其单个实例对象。
    
    可以在Java 7上使用，甚至在使用jsr166.jar的Java 6上使用。
    
    有关ForkJoinPool API及其递归行为(RecursiveActions)使用的详细信息，请参阅"JDK 7文档"。
    
    jsr166.jar，包含Java 6的并发包(java.util.concurrent)更新，可以从"并发兴趣站点"获取到。
    并发兴趣站点——http://gee.cs.oswego.edu/dl/concurrency-interest/
    
    public void setParallelism(int parallelism)——指定并发级别，默认是可用的处理器数量。
    
    public void setThreadFactory(ForkJoinPool.ForkJoinWorkerThreadFactory
    threadFactory)——设置创建新的"Fork-Join"工作者线程的工厂。
    默认与"Fork-Join"任务池(ForkJoinPool)使用一样的工厂
    
    public void setUncaughtExceptionHandler(Thread.UncaughtExceptionHandler
    uncaughtExceptionHandler)——设置内部工作者线程由于在执行任务时遇到不可恢复的错误而终止的处理器。
    默认是空
    
    public void setAsyncMode(boolean asyncMode)——指定是否为分开的任务建立一个本地的FIFO调度模式。
    该模式设置为{@code true}可能更合适，这样工作者线程只会处理"事件方式"的异步任务。
    
    public void afterPropertiesSet()——初始化"Fork-Join"任务池，保证在Spring
    IoC容器管理下仅有一个实例对象(单实例)。
    
    public boolean isSingleton()——返回true(单实例)。
    
    public void destroy()——关闭"Fork-Join"任务池。

--- src/main/java/org/springframework/scheduling/concurrent/ForkJoinPoolFactoryBean.java
+++ src/main/java/org/springframework/scheduling/concurrent/ForkJoinPoolFactoryBean.java
@@ -23,7 +23,19 @@ import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
- * A Spring {@link FactoryBean} that builds and exposes a preconfigured {@link ForkJoinPool}.
+ * 一个构建并暴露一个预先配置的"Fork-Join"任务池({@link ForkJoinPool})的
+ * Spring工厂Bean({@link FactoryBean})。
+ * 可以在Java 7上使用，甚至在使用{@code jsr166.jar}的Java 6上使用。
+ * 继承自{@link FactoryBean<ForkJoinPool>}
+ * 
+ * <p>有关ForkJoinPool API及其递归行为(RecursiveActions)使用的详细信息，请参阅
+ * <a href="http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ForkJoinPool.html">"JDK 7文档"</a>。
+ * 
+ * <p>{@code jsr166.jar}，包含Java 6的并发包({@code java.util.concurrent})更新，
+ * 可以从<a href="http://gee.cs.oswego.edu/dl/concurrency-interest/">"并发兴趣站点"</a>
+ * 获取到。
+ * 
+ * <p>A Spring {@link FactoryBean} that builds and exposes a preconfigured {@link ForkJoinPool}.
  * May be used on Java 7 as well as on Java 6 with {@code jsr166.jar} on the classpath
  * (ideally on the VM bootstrap classpath).
  *
@@ -38,26 +50,37 @@ import org.springframework.beans.factory.InitializingBean;
  */
 public class ForkJoinPoolFactoryBean implements FactoryBean<ForkJoinPool>, InitializingBean, DisposableBean {
 
+	// 用于运行Java虚拟机的处理器数量
 	private int parallelism = Runtime.getRuntime().availableProcessors();
 
+	// "Fork-Join"工作者线程工厂，默认与"Fork-Join"任务池使用一样的工厂
 	private ForkJoinPool.ForkJoinWorkerThreadFactory threadFactory = ForkJoinPool.defaultForkJoinWorkerThreadFactory;
 
+	// 未捕获的异常处理器
 	private Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
 
+	// 是否使用异步模式
 	private boolean asyncMode = false;
 
+
+	// [背后实现] "Fork-Join"任务池
 	private ForkJoinPool forkJoinPool;
 
 
 	/**
-	 * Specify the parallelism level. Default is {@link Runtime#availableProcessors()}.
+	 * 指定并发级别，默认是可用的处理器数量。
+	 * 
+	 * <p>Specify the parallelism level. Default is {@link Runtime#availableProcessors()}.
 	 */
 	public void setParallelism(int parallelism) {
 		this.parallelism = parallelism;
 	}
 
 	/**
-	 * Set the factory for creating new ForkJoinWorkerThreads.
+	 * 设置创建新的"Fork-Join"工作者线程的工厂。
+	 * 默认与"Fork-Join"任务池(ForkJoinPool)使用一样的工厂
+	 * 
+	 * <p>Set the factory for creating new ForkJoinWorkerThreads.
 	 * Default is {@link ForkJoinPool#defaultForkJoinWorkerThreadFactory}.
 	 */
 	public void setThreadFactory(ForkJoinPool.ForkJoinWorkerThreadFactory threadFactory) {
@@ -65,7 +88,10 @@ public class ForkJoinPoolFactoryBean implements FactoryBean<ForkJoinPool>, Initi
 	}
 
 	/**
-	 * Set the handler for internal worker threads that terminate due to unrecoverable errors
+	 * 设置内部工作者线程由于在执行任务时遇到不可恢复的错误而终止的处理器。
+	 * 默认是空
+	 * 
+	 * <p>Set the handler for internal worker threads that terminate due to unrecoverable errors
 	 * encountered while executing tasks. Default is none.
 	 */
 	public void setUncaughtExceptionHandler(Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
@@ -73,7 +99,10 @@ public class ForkJoinPoolFactoryBean implements FactoryBean<ForkJoinPool>, Initi
 	}
 
 	/**
-	 * Specify whether to establish a local first-in-first-out scheduling mode for forked tasks
+	 * 指定是否为分开的任务建立一个本地的FIFO调度模式。
+	 * 该模式设置为{@code true}可能更合适，这样工作者线程只会处理"事件方式"的异步任务。
+	 * 
+	 * <p>Specify whether to establish a local first-in-first-out scheduling mode for forked tasks
 	 * that are never joined. This mode (asyncMode = {@code true}) may be more appropriate
 	 * than the default locally stack-based mode in applications in which worker threads only
 	 * process event-style asynchronous tasks. Default is {@code false}.
@@ -82,25 +111,40 @@ public class ForkJoinPoolFactoryBean implements FactoryBean<ForkJoinPool>, Initi
 		this.asyncMode = asyncMode;
 	}
 
+	// # 初始化Bean(InitializingBean)
+	/**
+	 * 初始化"Fork-Join"任务池，保证在Spring IoC容器管理下仅有一个实例对象(单实例)。
+	 */
+	@Override
 	public void afterPropertiesSet() {
+		// 保证在Spring IoC容器管理下，只会被调用一次，即仅有一个实例对象(单实例)
 		this.forkJoinPool =
 				new ForkJoinPool(this.parallelism, this.threadFactory, this.uncaughtExceptionHandler, this.asyncMode);
 	}
 
 
+	// # 工厂Bean(FactoryBean)
+	@Override
 	public ForkJoinPool getObject() {
 		return this.forkJoinPool;
 	}
 
+	@Override
 	public Class<?> getObjectType() {
 		return ForkJoinPool.class;
 	}
 
+	@Override
 	public boolean isSingleton() {
-		return true;
+		return true; // 单实例
 	}
 
 
+	// # 销毁Bean(DisposableBean)
+	/**
+	 * 关闭"Fork-Join"任务池。
+	 */
+	@Override
 	public void destroy() {
 		this.forkJoinPool.shutdown();
 	}
