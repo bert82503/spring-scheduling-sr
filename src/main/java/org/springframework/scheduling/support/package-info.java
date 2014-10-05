01b9e532429dbabf0def2b8a9cdf66ebf50a7cf9
Edward Lee <bert825_work@163.com>
2014-10-05 22:30:58 +0800

    [译] org.springframework.scheduling.support——定义调度的通用支持类
    为Spring方法调用者(MethodInvoker)提供一个可运行任务的适配器
    
    CronSequenceGenerator：一个Crontab模式的时间序列生成器
    CronTrigger：cron表达式的触发器实现
    
    MethodInvokingRunnable：实现可运行接口作为一个基于Spring的方法调用者(MethodInvoker)的可配置方法调用的适配器
    PeriodicTrigger：一个周期性任务执行的触发器
    ScheduledMethodRunnable：MethodInvokingRunnable的变形
    SimpleTriggerContext：实现TriggerContext接口的简单的数据持有�
    TaskUtils：装饰任务错误处理的工具方法

--- src/main/java/org/springframework/scheduling/support/package-info.java
+++ src/main/java/org/springframework/scheduling/support/package-info.java
@@ -1,7 +1,19 @@
 
 /**
+ * 定义调度的通用支持类，
+ * 并为Spring方法调用者(MethodInvoker)提供一个可运行任务的适配器。
+ * 
+ * <ul>
+ * <li>CronSequenceGenerator：一个Crontab模式的时间序列生成器
+ * <li>CronTrigger：cron表达式的触发器实现
+ * <li>MethodInvokingRunnable：实现可运行接口作为一个基于Spring的方法调用者(MethodInvoker)的可配置方法调用的适配器
+ * <li>PeriodicTrigger：一个周期性任务执行的触发器
+ * <li>ScheduledMethodRunnable：MethodInvokingRunnable的变形
+ * <li>SimpleTriggerContext：实现TriggerContext接口的简单的数据持有者
+ * <li>TaskUtils：装饰任务错误处理的工具方法
+ * </ul>
  *
- * Generic support classes for scheduling.
+ * <p>Generic support classes for scheduling.
  * Provides a Runnable adapter for Spring's MethodInvoker.
  *
  */
