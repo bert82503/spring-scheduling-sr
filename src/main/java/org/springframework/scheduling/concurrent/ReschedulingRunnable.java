f67c91ea54c64a8cefe3d88835c4b4d780c260c4
Edward Lee <bert825_work@163.com>
2014-10-06 23:54:41 +0800

    ReschedulingRunnable——一个根据给定的触发器(Trigger)建议的下一次执行时间来重复调度背后的可运行任务的内部适配器
    这是必需的，因为一个原生的"调度的执行器服务(ScheduledExecutorService)"只支持"时延驱动"执行。
    触发器接口(Trigger)的灵活性是它能重复确定下一次执行时间的时延。
    (整合ScheduledExecutorService和Trigger功能)
    
    继承自"能捕获任何异常或错误的Runnable包装器(DelegatingErrorHandlingRunnable)"，并实现"可时延调度的Future(ScheduledFuture<Object>)"接口
    
    public ReschedulingRunnable(Runnable delegate, Trigger trigger,
    ScheduledExecutorService executor, ErrorHandler errorHandler)
    ——实例化一个可重复调度的可运行任务。
    
    public ScheduledFuture<Object> schedule()——调度任务。
    
    public void run()——使用"加锁机制"更新"触发器上下文"信息，并在"可时延调度的Future"未取消之前调度它。

--- src/main/java/org/springframework/scheduling/concurrent/ReschedulingRunnable.java
+++ src/main/java/org/springframework/scheduling/concurrent/ReschedulingRunnable.java
@@ -30,7 +30,17 @@ import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.util.ErrorHandler;
 
 /**
- * Internal adapter that reschedules an underlying {@link Runnable} according
+ * 一个根据给定的触发器({@link Trigger})建议的下一次执行时间
+ * 来重复调度背后的可运行任务的内部适配器。
+ * 
+ * <p>这是必需的，因为一个原生的"调度的执行器服务({@link ScheduledExecutorService})"
+ * 只支持"时延驱动"执行。触发器接口({@link Trigger})的灵活性是它能重复确定
+ * 下一次执行时间的时延。(整合ScheduledExecutorService和Trigger功能)
+ * 
+ * <p>继承自"能捕获任何异常或错误的Runnable包装器(DelegatingErrorHandlingRunnable)"，
+ * 并实现"可时延调度的Future(ScheduledFuture<Object>)"接口。
+ * 
+ * <p>Internal adapter that reschedules an underlying {@link Runnable} according
  * to the next execution time suggested by a given {@link Trigger}.
  *
  * <p>Necessary because a native {@link ScheduledExecutorService} supports
@@ -43,44 +53,79 @@ import org.springframework.util.ErrorHandler;
  */
 class ReschedulingRunnable extends DelegatingErrorHandlingRunnable implements ScheduledFuture<Object> {
 
+	// [core] 触发器(确定任务的下一次执行时间)
 	private final Trigger trigger;
 
+	// 触发器上下文
 	private final SimpleTriggerContext triggerContext = new SimpleTriggerContext();
 
-	private final ScheduledExecutorService executor;
-
-	private ScheduledFuture currentFuture;
-
-	private Date scheduledExecutionTime;
-
+	// 触发器上下文的监视对象
 	private final Object triggerContextMonitor = new Object();
 
+	// 下一次调度的执行时间
+	private Date scheduledExecutionTime;
 
-	public ReschedulingRunnable(Runnable delegate, Trigger trigger, ScheduledExecutorService executor, ErrorHandler errorHandler) {
+
+	// [core] 调度的执行器服务(JDK)
+	private final ScheduledExecutorService executor;
+
+	// 可时延调度的Future
+	private ScheduledFuture<Object> currentFuture;
+
+
+	/**
+	 * 实例化一个可重复调度的可运行任务。
+	 * 
+	 * @param delegate
+	 * @param trigger
+	 * @param executor
+	 * @param errorHandler
+	 */
+	public ReschedulingRunnable(Runnable delegate, Trigger trigger,
+			ScheduledExecutorService executor, ErrorHandler errorHandler) {
+		// 实例化一个"能捕获任何异常或错误的Runnable包装器"
 		super(delegate, errorHandler);
+		
 		this.trigger = trigger;
 		this.executor = executor;
 	}
 
 
-	public ScheduledFuture schedule() {
-		synchronized (this.triggerContextMonitor) {
+	/**
+	 * 调度任务。
+	 *
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public ScheduledFuture<Object> schedule() {
+		synchronized (this.triggerContextMonitor) { // 加锁同步
+			// 确定任务的下一次执行时间
 			this.scheduledExecutionTime = this.trigger.nextExecutionTime(this.triggerContext);
 			if (this.scheduledExecutionTime == null) {
 				return null;
 			}
+			// 第一次调度的时延
 			long initialDelay = this.scheduledExecutionTime.getTime() - System.currentTimeMillis();
-			this.currentFuture = this.executor.schedule(this, initialDelay, TimeUnit.MILLISECONDS);
+			this.currentFuture = (ScheduledFuture<Object>) 
+					this.executor.schedule(this, initialDelay, TimeUnit.MILLISECONDS);
 			return this;
 		}
 	}
 
+	// DelegatingErrorHandlingRunnable
+	/*
+	 * 使用"加锁机制"更新"触发器上下文"信息，并在"可时延调度的Future"未取消之前调度它。
+	 */
 	@Override
 	public void run() {
+		// 实际的执行时间
 		Date actualExecutionTime = new Date();
 		super.run();
+		// 完成的执行时间
 		Date completionTime = new Date();
+		
 		synchronized (this.triggerContextMonitor) {
+			// 更新"触发器上下文"信息
 			this.triggerContext.update(this.scheduledExecutionTime, actualExecutionTime, completionTime);
 			if (!this.currentFuture.isCancelled()) {
 				schedule();
@@ -89,54 +134,64 @@ class ReschedulingRunnable extends DelegatingErrorHandlingRunnable implements Sc
 	}
 
 
+	// Future implementation (全部委托给currentFuture属性处理)
+	@Override
 	public boolean cancel(boolean mayInterruptIfRunning) {
-		synchronized (this.triggerContextMonitor) {
+		synchronized (this.triggerContextMonitor) { // 加锁同步
 			return this.currentFuture.cancel(mayInterruptIfRunning);
 		}
 	}
 
+	@Override
 	public boolean isCancelled() {
 		synchronized (this.triggerContextMonitor) {
 			return this.currentFuture.isCancelled();
 		}
 	}
 
+	@Override
 	public boolean isDone() {
 		synchronized (this.triggerContextMonitor) {
 			return this.currentFuture.isDone();
 		}
 	}
 
+	@Override
 	public Object get() throws InterruptedException, ExecutionException {
-		ScheduledFuture curr;
+		ScheduledFuture<Object> curr;
 		synchronized (this.triggerContextMonitor) {
 			curr = this.currentFuture;
 		}
 		return curr.get();
 	}
 
+	@Override
 	public Object get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-		ScheduledFuture curr;
+		ScheduledFuture<Object> curr;
 		synchronized (this.triggerContextMonitor) {
 			curr = this.currentFuture;
 		}
 		return curr.get(timeout, unit);
 	}
 
+
+	// Delayed implementation
+	@Override
 	public long getDelay(TimeUnit unit) {
-		ScheduledFuture curr;
+		ScheduledFuture<Object> curr;
 		synchronized (this.triggerContextMonitor) {
 			curr = this.currentFuture;
 		}
 		return curr.getDelay(unit);
 	}
 
+	@Override
 	public int compareTo(Delayed other) {
 		if (this == other) {
 			return 0;
 		}
 		long diff = getDelay(TimeUnit.MILLISECONDS) - other.getDelay(TimeUnit.MILLISECONDS);
-		return (diff == 0 ? 0 : ((diff < 0)? -1 : 1));
+		return (diff == 0 ? 0 : ((diff < 0) ? -1 : 1));
 	}
 
 }
