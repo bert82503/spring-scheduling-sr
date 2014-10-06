bdb4708e69be083ca0bf69d57bcf6ee243f356cd
Edward Lee <bert825_work@163.com>
2014-10-06 21:13:48 +0800

    TaskUtils——装饰"任务错误处理"的工具方法
    注意：本类仅供"Spring调度器实现类"内部使用。
    
    public static DelegatingErrorHandlingRunnable
    decorateTaskWithErrorHandler(
    Runnable task, ErrorHandler errorHandler, boolean isRepeatingTask)
    ——装饰能"处理错误"的任务。
    如果给定的错误执行器不是null，则它会被使用。
    
    public static ErrorHandler getDefaultErrorHandler(boolean
    isRepeatingTask)——基于"是否重复任务"来返回默认的"错误处理器(ErrorHandler)"实现。
    
    [内部静态类]
    LoggingErrorHandler——"在错误级别记录异常信息"的错误处理器实现
    当错误是预期的行为时，会有用。(任务会接着继续执行)
    
    PropagatingErrorHandler——"在错误级别记录异常信息，然后重新抛出该异常"的错误处理器实现
    阻止任务继续执行

--- src/main/java/org/springframework/scheduling/support/TaskUtils.java
+++ src/main/java/org/springframework/scheduling/support/TaskUtils.java
@@ -25,7 +25,11 @@ import org.springframework.util.ErrorHandler;
 import org.springframework.util.ReflectionUtils;
 
 /**
- * Utility methods for decorating tasks with error handling.
+ * 装饰"任务错误处理"的工具方法。
+ * 
+ * <p><b>注意：</b>本类仅供"Spring调度器实现类"内部使用。
+ * 
+ * <p>Utility methods for decorating tasks with error handling.
  *
  * <p><b>NOTE:</b> This class is intended for internal use by Spring's scheduler
  * implementations. It is only public so that it may be accessed from
@@ -38,14 +42,20 @@ import org.springframework.util.ReflectionUtils;
 public abstract class TaskUtils {
 
 	/**
-	 * An ErrorHandler strategy that will log the Exception but perform
+	 * "记录异常信息"的错误处理策略。
+	 * 任务会接着继续执行
+	 * 
+	 * <p>An ErrorHandler strategy that will log the Exception but perform
 	 * no further handling. This will suppress the error so that
 	 * subsequent executions of the task will not be prevented.
 	 */
 	public static final ErrorHandler LOG_AND_SUPPRESS_ERROR_HANDLER = new LoggingErrorHandler();
 
 	/**
-	 * An ErrorHandler strategy that will log at error level and then
+	 * "记录错误级别信息，然后重新抛出异常"的错误处理策略。
+	 * 阻止任务继续执行
+	 * 
+	 * <p>An ErrorHandler strategy that will log at error level and then
 	 * re-throw the Exception. Note: this will typically prevent subsequent
 	 * execution of a scheduled task.
 	 */
@@ -53,6 +63,9 @@ public abstract class TaskUtils {
 
 
 	/**
+	 * <p>装饰能"处理错误"的任务。
+	 * 如果给定的错误执行器不是null，则它会被使用。
+	 * 
 	 * Decorates the task for error handling. If the provided
 	 * {@link ErrorHandler} is not null, it will be used. Otherwise,
 	 * repeating tasks will have errors suppressed by default whereas
@@ -71,7 +84,9 @@ public abstract class TaskUtils {
 	}
 
 	/**
-	 * Return the default {@link ErrorHandler} implementation based on the boolean
+	 * 基于"是否重复任务"来返回默认的"错误处理器({@link ErrorHandler})"实现。
+	 * 
+	 * <p>Return the default {@link ErrorHandler} implementation based on the boolean
 	 * value indicating whether the task will be repeating or not. For repeating tasks
 	 * it will suppress errors, but for one-time tasks it will propagate. In both
 	 * cases, the error will be logged.
@@ -82,7 +97,10 @@ public abstract class TaskUtils {
 
 
 	/**
-	 * An {@link ErrorHandler} implementation that logs the Throwable at error
+	 * "在错误级别记录异常信息"的错误处理器实现。
+	 * 当错误是预期的行为时，会有用。(任务会接着继续执行)
+	 * 
+	 * <p>An {@link ErrorHandler} implementation that logs the Throwable at error
  	 * level. It does not perform any additional error handling. This can be
  	 * useful when suppression of errors is the intended behavior.
 	 */
@@ -99,7 +117,10 @@ public abstract class TaskUtils {
 
 
 	/**
-	 * An {@link ErrorHandler} implementation that logs the Throwable at error
+	 * "在错误级别记录异常信息，然后重新抛出该异常"的错误处理器实现。
+	 * 阻止任务继续执行
+	 * 
+	 * <p>An {@link ErrorHandler} implementation that logs the Throwable at error
 	 * level and then propagates it.
 	 */
 	static class PropagatingErrorHandler extends LoggingErrorHandler {
